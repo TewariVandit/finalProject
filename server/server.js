c824b12b19b0b8453c8d18ef1398b1877200882f
TewariVandit <89832457+TewariVandit@users.noreply.github.com>
2026-05-09 12:28:05 +0530

    add vercel single deploy config

--- server/server.js
+++ server/server.js
@@ -64,6 +64,10 @@ app.get(/.*/, (req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (process.env.VERCEL !== "1") {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
